46fee4df553549626a883340b979681e8ca52984
Fayaz <flalani100@gmail.com>
2022-03-09 13:53:21 -0500

    Updated Thought model

--- models/Thought.js
+++ models/Thought.js
@@ -15,8 +15,28 @@ const ThoughtSchema = newSchema (
             type: Date,
             default: Date.now,
             get: createdAtVal => dateFormat(createdAtVal)
-        }
+        },
 
+        username: {
+            type: String,
+            require: true
+        },
+        reactions: [reactionCount]
 
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
     }
 )
+
+ThoughtSchema.virtuals('reactionCount').get(function(){
+    return this.reactions.length;
+})
+
+const User = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
