cd3557ea4f64b836435ebe662c8611dce1cad053
Fayaz <flalani100@gmail.com>
2022-03-09 15:13:15 -0500

    Moved user routes into correct file

---
+++ controllers/user-controller.js
@@ -0,0 +1,24 @@
+const { User, Thought } = require('../models');
+
+const userController = {
+    //get all users
+        getAllUsers(req, res){
+            User.find({})
+            .populate({
+                path: 'thoughts',
+                select: '-__v'
+            })
+            .select('-__v')
+            .sort({_id: -1})
+            .then(dbUserData => {
+                console.log(dbUserData);
+                if(dbUserData){
+                    res.status(404).json({ message: 'No user found with this id'});
+                    return
+                }
+                res.json(dbUserData)
+            })
+            .catch(err => res.json)
+        }
+
+}
