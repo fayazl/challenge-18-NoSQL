4c3c50c07a6a071d89d47cf3cddaec955e991cbc
Fayaz <flalani100@gmail.com>
2022-03-09 09:57:30 -0500

    Updated user model

--- models/User.js
+++ models/User.js
@@ -15,6 +15,31 @@ const UserSchema = new Schema(
             required: true,
             unique: true,
             match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please fill a valid email address']
-        }
+        },
+
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }],
+
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
     }
 )
+
+UserSchema.virtual('friendCount').get(function(){
+    return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
