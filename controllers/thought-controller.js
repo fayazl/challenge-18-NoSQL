f9d8d0162a3fb567481f8b20ae9fc21ab64b8587
Fayaz <flalani100@gmail.com>
2022-03-10 00:12:33 -0500

    UpdateThoughts method in controllers - tested and validated

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -41,9 +41,20 @@ const thoughtController = {
                 res.json(dbThoughtData);
               })
               .catch(err => res.json(err));
-    }
+    },
 
-    updateThoughts ()
+    updateThoughts({params, body}, res){
+        Thought.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+              res.status(404).json({ message: 'No thoughts found with this id' });
+              return;
+            }
+            res.json(dbThoughtData);
+          })
+          .catch(err => res.json(err));
+    },
+
 }
 
 module.exports = thoughtController;
