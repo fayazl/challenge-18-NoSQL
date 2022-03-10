b51525138895e43961a9981bab2fd123edd9eb8c
Fayaz <flalani100@gmail.com>
2022-03-09 23:39:03 -0500

    Added thought controller get all and findbyid

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,1 +1,24 @@
-const { User } = require('../models');
+const { Thought, User } = require('../models');
+
+const thoughtController = {
+    getAllThoughts(req, res){
+        Thought.find({})
+        .select('-__v')
+        .sort({_id: -1})
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+        });
+    },
+
+    getThoughtsById({ params}, res){
+        Thought.findOne({_id: params.id})
+        .select('-__v')
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400)
+        });
+    }
+}
